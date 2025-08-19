253f65820efec4416a1ee7391fae5267be39af55
talhaarain666 <talhaarain666@gmail.com>
2025-08-19 01:08:41 -0700

    express js home page and news api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Note: nodemon package for auto-reloading during development
 // Set-ExecutionPolicy RemoteSigned -Scope CurrentUser (for permission issues on Windows)
 //  commonjs in package.json for Node.js compatibility (now we use require) if we not define then bydefault it is commonjs
-
+// start script in package.json: "start": "nodemon index.js", now we can run the server using `npm start`
 
 // ....................................................... CLASS 1 ............................................
 // /////////////////////////////////////////////////////// MODULES in Node.js
@@ -53,4 +53,20 @@
 // ....................................................... CLASS 3 ............................................
 /////////////////////////////////////////////////////////// Create Server with Express.js
 
-// let express = require("express")
+let express = require("express");
+
+let app = express();
+
+app.get("/",(req,res)=>{ // http://localhost:8000/
+    // no need of json.stringify , express does it automatically
+    res.send({
+        status: 1,
+        message: "Home page api"
+    });
+})
+
+app.get("/news",(req,res)=>{
+    res.send({status:1,message:"News page api"})
+})
+
+app.listen(8000)
