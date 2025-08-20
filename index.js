5e529256e3d3860e5c2653a9eebee5582c00a439
talhaarain666 <talhaarain666@gmail.com>
2025-08-20 07:46:59 -0700

    post api, dynamic routing

--- index.js
+++ index.js
@@ -40,10 +40,10 @@
 
 //     if(req.url == "/about") { // http://localhost:8000/about 
 //     }
-    
+
 //     if(req.url == "/") { // http://localhost:8000
 //     }
-    
+
 //     res.end("Welcome to the Node.js Server!");
 // });
 
@@ -56,8 +56,9 @@
 let express = require("express");
 
 let app = express();
+app.use(express.json()); // to parse JSON data in request body
 
-app.get("/",(req,res)=>{ // http://localhost:8000/
+app.get("/", (req, res) => { // http://localhost:8000/
     // no need of json.stringify , express does it automatically
     res.send({
         status: 1,
@@ -65,8 +66,46 @@ app.get("/",(req,res)=>{ // http://localhost:8000/
     });
 })
 
-app.get("/news",(req,res)=>{
-    res.send({status:1,message:"News page api"})
+app.get("/news", (req, res) => {
+    res.send({ status: 1, message: "News page api" })
+})
+
+
+// ........................................................ CLASS 4 ............................................
+// /////////////////////////////////////////////////////// Dynamic Routes in Express.js
+
+
+// dynamic route with parameter eg: http://localhost:8000/news/1
+app.get("/news/:id", (req, res) => {
+    // req.params is an object containing the route parameters
+    console.log(req.params.id); // current id, '1' if URL is /news/1
+    res.send({
+        status: 1,
+        message: "News detail page api",
+        // data from request params
+        paramsData: req.params
+    })
+})
+
+app.post("/login", (req, res) => {
+    console.log(req.body); // always object
+
+    // if status is 200 then it is success
+    res.status(200).send({ // 200 is the status code for success
+        status: 1,
+        message: "Login successful",
+        // data from request body
+        bodyData: req.body,
+        // query parameters in URL (eg localhost:8000/login?username=abc&password=123) (query data used for searching, filtering, sorting, pagination etc.)
+        queryData: req.query
+    })
+
+    // res.send({
+    //     status: 1,
+    //     message: "Login page api",
+    //     bodyData: req.body,
+    //     queryData: req.query
+    // })
 })
 
 app.listen(8000)
